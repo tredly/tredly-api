30ac24d652a7bed42fd3960b72e31634e6e28696
Alistair Kearney <pointybeard@users.noreply.github.com>
2016-06-23 14:57:28 +1000

    PR from vuid-com (#47)
    * Change rc.d script location from /etc/rc.d to /usr/local/etc/rc.d
    * Add full paths to installer
    * Closes tredly/tredly-api#46 Accept password from input stream in "install.sh"
    * Change path to node

--- install.sh
+++ install.sh
@@ -10,16 +10,18 @@ then
    exit 1
 fi
 
+read -t 1 STDIN || true
+
 BINDIR="/usr/local/sbin/tredly-api"
 
 echo -e "\u001b[32m\u001b[1m################\u001b[22m\u001b[39m"
 echo -e "\u001b[32m\u001b[1m### Installing Tredly Host API... \u001b[22m\u001b[39m"
 
 echo -e "\u001b[32m\u001b[1m### Installing Node.js... \u001b[22m\u001b[39m"
-pkg install -y node
+/usr/sbin/pkg install -y node
 
 echo -e "\u001b[32m\u001b[1m### Installing NPM... \u001b[22m\u001b[39m"
-pkg install -y npm
+/usr/sbin/pkg install -y npm
 
 echo -e "\u001b[32m\u001b[1m### Moving installation folder... \u001b[22m\u001b[39m"
 rm -rf .git
@@ -33,10 +35,10 @@ cd "${BINDIR}/"
 pwd
 
 echo -e "\u001b[32m\u001b[1m### Installing dependencies... \u001b[22m\u001b[39m"
-npm install
+/usr/local/bin/npm install
 
 echo -e "\u001b[32m\u001b[1m### Installing Tredly Host API server... \u001b[22m\u001b[39m"
-API_PASSWORD="$(node ./lib/install.js --username=admin --password --port=65223 --ssl)"
+API_PASSWORD="$(/usr/local/bin/node ./lib/install.js --username=admin --password="$STDIN" --port=65223 --ssl)"
 echo -e "Your API password is: ${API_PASSWORD}"
 
 echo -e "\u001b[32m\u001b[1m### Starting Tredly Host API server... \u001b[22m\u001b[39m"
